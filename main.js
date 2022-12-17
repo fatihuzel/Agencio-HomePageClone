a31163bbd3a5e16bbfea025206426794fe091fc2
Fatih Uzel <pupsezolto@outlook.com>
2022-12-17 23:29:40 +0300

    file changes

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 // for navbar javascript codes
 window.addEventListener("DOMContentLoaded", (event) => {
-  var navbarMobile = function () {
+  const navbarMobile = function () {
     const nCollapsible = document.body.querySelector("#mainNavbar");
     if (!nCollapsible) {
       return;
+    }
+    const logoImg = document.querySelector("#test");
+    if (window.scrollY > 66) {
+      nCollapsible.classList.add("navbar-shrink");
+      logoImg.src = "images/logo.svg";
+    } else {
+      nCollapsible.classList.remove("navbar-shrink");
+      logoImg.src = "images/white-logo.svg";
     }
     if (window.scrollY === 0) {
       nCollapsible.classList.remove("navbar-mobile");
@@ -27,25 +35,3 @@ for (let i = 0; i < BtnCanvas.length; i++) {
     document.querySelector('[data-bs-dismiss="offcanvas"]').click();
   });
 }
-
-const imageGallery = document.querySelector('#img-filter');
-const filterOptions = imageGallery.querySelectorAll('.filter-options button');
-const parentinner = document.querySelectorAll('[data-category]');
-
-// const showImages = category => {
-//   parentinner.forEach(image => {
-//     if (image.getAttribute('data-category') === category || category === 'all') {
-//       image.style.display = 'block';
-//     } else {
-//       image.style.display = 'none';
-//     }
-//   });
-// }
-
-// filterOptions.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const category = button.getAttribute('data-filter');
-//     showImages(category);
-//   });
-// });
-
